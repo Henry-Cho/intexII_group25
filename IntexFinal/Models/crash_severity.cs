26d0244fbaf92654a53a61086c677006bad17cf6
delangea <73494880+delangea@users.noreply.github.com>
2022-04-04 16:18:59 -0600

    build models

---
+++ IntexFinal/Models/crash_severity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntexFinal.Models
+{
+    public class crash_severity
+    {
+        [Key]
+        [Required]
+        public int crash_severity_id { get; set; }
+        public string crash_severity_description { get; set; }
+    }
+}
