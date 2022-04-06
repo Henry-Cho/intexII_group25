1497fb5bb72632592f7d81dd44eb9824544516fb
dongkya <kdh2395478@gmail.com>
2022-04-06 10:20:37 -0600

    .
    pp

--- IntexFinal/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Admin/Editor.razor.g.cs
+++ IntexFinal/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Admin/Editor.razor.g.cs
@@ -12,42 +12,42 @@ namespace IntexFinal.Pages.Admin
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 1 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 2 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 3 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 4 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 6 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\_Imports.razor"
+#line 7 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/_Imports.razor"
 using IntexFinal.Models;
 
 #line default
@@ -63,7 +63,7 @@ using IntexFinal.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 193 "C:\Users\Ransom Allphin\source\repos\intexII_group25\IntexFinal\Pages\Admin\Editor.razor"
+#line 193 "/Users/dongking/Documents/GitHub/intexII_group25/IntexFinal/Pages/Admin/Editor.razor"
        
     [Parameter]
     public long Id { get; set; } = 0;
