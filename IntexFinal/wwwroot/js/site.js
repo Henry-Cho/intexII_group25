efdfb6a2d4ffab89ac5fa38d8ccb321c16e7ea16
delangea <73494880+delangea@users.noreply.github.com>
2022-04-06 10:18:22 -0600

    fix stuff

--- IntexFinal/wwwroot/js/site.js
+++ IntexFinal/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function ChangeCheckIntoFloat(name, id) {
+    var check = document.getElementsByName(name);
+    if (check[0].checked) {
+        var hiddenInput = document.getElementById(id);
+        hiddenInput.value = 1;
+    }
+    else {
+        var hiddenInput = document.getElementById(id);
+        hiddenInput.value = 0;
+    }
+
+}
+
+function ChangeSelectIntoFloat(id) {
+    var select = document.getElementById(id);
+    for (i = 0; i < select.length; i++) {
+        if (select.options[i].value == select.options[select.selectedIndex].value) {
+            if (select.options[i].value != "Default") {
+                var hidden = document.getElementById(select.options[i].value);
+                hidden.value = 1;
+            }
+        }
+        else {
+            if (select.options[i].value != "Default") {
+                var hidden = document.getElementById(select.options[i].value);
+                hidden.value = 0;
+            }
+        }
+    }
+
+}
