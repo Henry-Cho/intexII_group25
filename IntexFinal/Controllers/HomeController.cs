44b9f4feb520b18a2f1892ce85ecd8ca6d0ccf49
delangea <73494880+delangea@users.noreply.github.com>
2022-04-06 21:18:03 -0600

    get a lot of work done on the filtering

--- IntexFinal/Controllers/HomeController.cs
+++ IntexFinal/Controllers/HomeController.cs
@@ -9,16 +9,22 @@ using IntexFinal.Models;
 using IntexFinal.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.AspNetCore.Http;
 
 namespace IntexFinal.Controllers
 {
     public class HomeController : Controller
     {
         private ICrashRepository repo;
+        private InferenceSession _session;
 
-        public HomeController(ICrashRepository context)
+
+        public HomeController(ICrashRepository context, InferenceSession session)
         {
             repo = context;
+            _session = session;
         }
 
         public IActionResult Index()
@@ -48,14 +54,13 @@ namespace IntexFinal.Controllers
             return View(c);
         }
 
-        public IActionResult Incidents(int pageNum = 1)
+        public IActionResult Incidents(int pageNum = 1, List<crash_data> cd = null)
         {
-            //List<crash_data> cd = (List<crash_data>)TempData["record"];
             int pageSize = 7;
             var c = new IncidentsViewModel();
 
-            //if (cd?.Count() == null)
-            //{
+            if (cd.Count() == 0)
+            {
                 c = new IncidentsViewModel
                 {
                     Incidents = repo.Crash_Data
@@ -70,26 +75,24 @@ namespace IntexFinal.Controllers
                         CurrentPage = pageNum
                     }
                 };
-            //}
-            //else
-            //{
-            //    cd = (List<crash_data>)cd.AsQueryable();
-            //    c = new IncidentsViewModel
-            //    {
-            //        Incidents = (IQueryable<crash_data>)cd
-            //            .OrderByDescending(x => x.crash_datetime)
-            //            .Skip((pageNum - 1) * pageSize)
-            //            .Take(pageSize),
+            }
+            else
+            {
+                c = new IncidentsViewModel
+                {
+                    Incidents = cd.AsQueryable()
+                        .OrderByDescending(x => x.crash_datetime)
+                        .Skip((pageNum - 1) * pageSize)
+                        .Take(pageSize),
 
-            //        PageInfo = new PageInfo
-            //        {
-            //            TotalNumIncidents = cd.Count(),
-            //            IncidentsPerPage = pageSize,
-            //            CurrentPage = pageNum
-            //        }
-            //    };
-            //    ViewData["record"] = JsonConvert.DeserializeObject<List<crash_data>>((string)TempData["record"]);
-            //}
+                    PageInfo = new PageInfo
+                    {
+                        TotalNumIncidents = cd.Count(),
+                        IncidentsPerPage = pageSize,
+                        CurrentPage = pageNum
+                    }
+                };
+            }
 
             return View(c);
         }
@@ -121,21 +124,6 @@ namespace IntexFinal.Controllers
 
             ViewBag.city = ct;
 
-            var lux = repo.Crash_Data
-                .Select(x => x.long_utm_x)
-                .Distinct()
-                .OrderBy(x => x)
-                .ToList();
-
-            ViewBag.long_utm_x = csi;
-
-            var lay = repo.Crash_Data
-                .Select(x => x.lat_utm_y)
-                .Distinct()
-                .OrderBy(x => x)
-                .ToList();
-
-            ViewBag.lat_utm_y = lay;
 
             var mrn = repo.Crash_Data
                 .Select(x => x.main_road_name)
@@ -161,27 +149,169 @@ namespace IntexFinal.Controllers
 
             ViewBag.route = rt;
 
-            var cdt = repo.Crash_Data
-                .Select(x => x.crash_datetime)
-                .Distinct()
-                .OrderBy(x => x)
-                .ToList();
-
-            ViewBag.crash_datetime = cdt;
 
             return View();
         }
 
         [HttpPost]
-        public IActionResult Filter(crash_data cd)
+        public IActionResult Filter(IFormCollection form )
         {
-            var record = repo.GetFiltered(cd.city, cd.county_name, cd.crash_datetime, cd.route, cd.milepoint, cd.main_road_name, cd.lat_utm_y, cd.long_utm_x, cd.crash_severity_id,
-                cd.work_zone_related, cd.pedestrian_involved, cd.bicyclist_involved, cd.motorcycle_involved, cd.improper_restraint, cd.unrestrained, cd.dui, cd.intersection_related,
-                cd.wild_animal_related, cd.domestic_animal_related, cd.overturn_rollover, cd.commercial_motor_veh_involved,
-                cd.teenage_driver_involved, cd.older_driver_involved, cd.night_dark_condition, cd.single_vehicle, cd.distracted_driving, cd.drowsy_driving, cd.roadway_departure);
+            string? city = null;
+            string? county_name = null;
+            int? route = null; 
+            double? milepoint = null;
+            string? main_road_name = null;
+            int? crash_severity_id = null;
+            bool? work_zone_related = null;
+            bool? pedestrian_involved = null;
+            bool? bicyclist_involved = null;
+            bool? motorcycle_involved = null;
+            bool? improper_restraint = null;
+
+            bool? unrestrained = null;
+            bool? dui = null;
+            bool? intersection_related = null;
+            bool? wild_animal_related = null;
+            bool? domestic_animal_related = null;
+            bool? overturn_rollover = null;
+            bool? commercial_motor_veh_involved = null;
+
+            bool? teenage_driver_involved = null;
+            bool? older_driver_involved = null;
+            bool? night_dark_condition = null;
+            bool? single_vehicle = null;
+            bool? distracted_driving = null;
+            bool? drowsy_driving = null;
+            bool? roadway_departure = null;
+        
+            var route_temp = form["route"];
+            if(route_temp != "null")
+            {
+                route = Convert.ToInt32(route_temp);
+            }
+            var milepoint_temp = form["milepoint"];
+            if (milepoint_temp != "null")
+            {
+                milepoint = Convert.ToInt32(milepoint_temp);
+            }
+            var main_road_temp = form["main_road_name"];
+            if (main_road_temp != "null")
+            {
+                main_road_name = main_road_temp;
+            }
+            var city_temp = form["city"];
+            if (city_temp != "null")
+            {
+                city = city_temp;
+            }
+            var crash_severity_temp = form["crash_severity_id"];
+            if (crash_severity_temp != "null")
+            {
+                crash_severity_id = Convert.ToInt32(crash_severity_temp);
+            }
+
+            var work_temp = form["work_zone_related"];
+            if (work_temp != "null")
+            {
+                work_zone_related = Convert.ToBoolean(work_temp);
+            }
+            var pedestrian_temp= form["pedestrian_involved"];
+            if (pedestrian_temp != "null")
+            {
+                pedestrian_involved = Convert.ToBoolean(pedestrian_temp);
+            }
+            var bicyclist_temp = form["bicyclist_involved"];
+            if (bicyclist_temp != "null")
+            {
+                bicyclist_involved = Convert.ToBoolean(bicyclist_temp);
+            }
+            var motorcycle_temp = form["motorcycle_involved"];
+            if (motorcycle_temp != "null")
+            {
+                motorcycle_involved = Convert.ToBoolean(motorcycle_temp);
+            }
+            var improper_temp = form["improper_restraint"];
+            if (improper_temp != "null")
+            {
+                improper_restraint = Convert.ToBoolean(improper_temp);
+            }
+            var unrestrained_temp = form["unrestrained"];
+            if (unrestrained_temp != "null")
+            {
+                unrestrained = Convert.ToBoolean(unrestrained_temp);
+            }
+            var dui_temp = form["dui"];
+            if (dui_temp != "null")
+            {
+                dui = Convert.ToBoolean(dui_temp);
+            }
+            var intersection_temp = form["intersection_related"];
+            if (intersection_temp != "null")
+            {
+                intersection_related = Convert.ToBoolean(intersection_temp);
+            }
+            var wild_animal_temp = form["wild_animal_related"];
+            if (wild_animal_temp != "null")
+            {
+                wild_animal_related = Convert.ToBoolean(wild_animal_temp);
+            }
+            var domestic_animal_temp = form["domestic_animal_related"];
+            if (domestic_animal_temp != "null")
+            {
+                domestic_animal_related = Convert.ToBoolean(domestic_animal_temp);
+            }
+            var overturn_temp = form["overturn_rollover"];
+            if (overturn_temp != "null")
+            {
+                overturn_rollover = Convert.ToBoolean(overturn_temp);
+            }
+            var commercial_temp = form["commercial_motor_veh_involved"];
+            if (commercial_temp != "null")
+            {
+                commercial_motor_veh_involved = Convert.ToBoolean(commercial_temp);
+            }
+            var teenage_driver_temp = form["teenage_driver_involved"];
+            if (teenage_driver_temp != "null")
+            {
+                teenage_driver_involved = Convert.ToBoolean(teenage_driver_temp);
+            }
+            var older_driver_temp = form["older_driver_involved"];
+            if (older_driver_temp != "null")
+            {
+                older_driver_involved = Convert.ToBoolean(older_driver_temp);
+            }
+            var night_temp = form["night_dark_condition"];
+            if (night_temp != "null")
+            {
+                night_dark_condition = Convert.ToBoolean(night_temp);
+            }
+            var single_temp= form["single_vehicle"];
+            if (single_temp != "null")
+            {
+                single_vehicle = Convert.ToBoolean(single_temp);
+            }
+            var distracted_temp= form["distracted_driving"];
+            if (distracted_temp != "null")
+            {
+                distracted_driving = Convert.ToBoolean(distracted_temp);
+            }
+            var drowsy_temp= form["drowsy_driving"];
+            if (drowsy_temp != "null")
+            {
+                drowsy_driving = Convert.ToBoolean(drowsy_temp);
+            }
+            var roadway_temp = form["roadway_departure"];
+            if (roadway_temp != "null")
+            {
+               roadway_departure = Convert.ToBoolean(roadway_temp);
+            }
+
+
+            List<crash_data> record = repo.GetFiltered(city, county_name, route, milepoint, main_road_name, crash_severity_id,   work_zone_related,   pedestrian_involved,   bicyclist_involved,   motorcycle_involved,   improper_restraint,
+              unrestrained,   dui,   intersection_related,   wild_animal_related,   domestic_animal_related,   overturn_rollover,   commercial_motor_veh_involved,
+              teenage_driver_involved,   older_driver_involved,   night_dark_condition,   single_vehicle,   distracted_driving,   drowsy_driving,   roadway_departure);
                
-            //TempData["record"] = JsonConvert.SerializeObject(record);
-            return RedirectToAction("FilteredIncidents", new { cd = record, pageNum = 1});
+            return RedirectToAction("Incidents", new { pageNum = 1, cd = record});
         }
 
         [HttpGet]
@@ -192,13 +322,90 @@ namespace IntexFinal.Controllers
         [HttpGet]
         public IActionResult FullCalculator()
         {
-            crash_data cd = new crash_data();
+            crash_prediction cd = new crash_prediction();
             return View(cd);
         }
         [HttpPost]
-        public IActionResult FullCalculator(crash_data cd)
+        public IActionResult FullCalculator(crash_prediction data)
         {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First()};
+            result.Dispose();
+            return RedirectToAction("Result", prediction);
+        }
+        [HttpGet]
+        public IActionResult CityCalc()
+        {
+            crash_prediction cd = new crash_prediction();
             return View(cd);
+        }
+        [HttpPost]
+        public IActionResult CityCalc(crash_prediction data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return RedirectToAction("Result", prediction);
+        }
+        [HttpGet]
+        public IActionResult DistCalc()
+        {
+            crash_prediction cd = new crash_prediction();
+            return View(cd);
+        }
+        [HttpPost]
+        public IActionResult DistCalc(crash_prediction data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return RedirectToAction("Result", prediction);
+        }
+        [HttpGet]
+        public IActionResult SpecialCalc()
+        {
+            crash_prediction cd = new crash_prediction();
+            return View(cd);
+        }
+        [HttpPost]
+        public IActionResult SpecialCalc(crash_prediction data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() };
+            result.Dispose();
+            return RedirectToAction("Result", prediction);
+        }
+        public IActionResult Result(Prediction p)
+        {
+            if (p.PredictedValue > 5)
+            {
+                p.PredictedValue = 5.0f;
+            }
+            if(p.PredictedValue < 1)
+            {
+                p.PredictedValue = 1.0f;
+            }
+            return View(p);
+        }
+        public IActionResult Stats()
+        {
+            return View();
         }
 
 
--- IntexFinal/Models/EFCrashRepository.cs
+++ IntexFinal/Models/EFCrashRepository.cs
@@ -35,40 +35,62 @@ namespace IntexFinal.Models
             _context.SaveChanges();
         }
 
-        public IQueryable<crash_data> GetFiltered(string? city, string? county_name, DateTime crash_datetime, int route, double milepoint, string main_road_name, double lat_utm_y, double long_utm_x, int crash_severity_id, bool work_zone_related, bool pedestrian_involved, bool bicyclist_involved, bool motorcycle_involved, bool improper_restraint,
-            bool unrestrained, bool dui, bool intersection_related, bool wild_animal_related, bool domestic_animal_related, bool overturn_rollover, bool commercial_motor_veh_involved,
-            bool teenage_driver_involved, bool older_driver_involved, bool night_dark_condition, bool single_vehicle, bool distracted_driving, bool drowsy_driving, bool roadway_departure)
+        public List<crash_data> GetFiltered(string? city, string? county_name,  int? route, double? milepoint, string? main_road_name, int? crash_severity_id, bool? work_zone_related, bool? pedestrian_involved, bool? bicyclist_involved, bool? motorcycle_involved, bool? improper_restraint,
+            bool? unrestrained, bool? dui, bool? intersection_related, bool? wild_animal_related, bool? domestic_animal_related, bool? overturn_rollover, bool? commercial_motor_veh_involved,
+            bool? teenage_driver_involved, bool? older_driver_involved, bool? night_dark_condition, bool? single_vehicle, bool? distracted_driving, bool? drowsy_driving, bool? roadway_departure)
         {
-                var record = _context.Crash_Data.Where(x => x.bicyclist_involved == bicyclist_involved)
-                .Where(x => x.city == city || city == null)
-                .Where(x => x.commercial_motor_veh_involved == commercial_motor_veh_involved)
-                .Where(x => x.county_name == county_name || county_name == null)
-                .Where(x => x.crash_datetime == crash_datetime || crash_datetime == null)
-                .Where(x => x.crash_severity_id == crash_severity_id)
-                .Where(x => x.distracted_driving == distracted_driving)
-                .Where(x => x.domestic_animal_related == domestic_animal_related)
-                .Where(x => x.drowsy_driving == drowsy_driving)
-                .Where(x => x.dui == dui)
-                .Where(x => x.improper_restraint == improper_restraint)
-                .Where(x => x.intersection_related == intersection_related)
-                .Where(x => x.lat_utm_y == lat_utm_y)
-                .Where(x => x.long_utm_x == long_utm_x)
-                .Where(x => x.main_road_name == main_road_name || main_road_name == null)
-                .Where(x => x.milepoint == milepoint)
-                .Where(x => x.motorcycle_involved == motorcycle_involved)
-                .Where(x => x.night_dark_condition == night_dark_condition)
-                .Where(x => x.older_driver_involved == older_driver_involved)
-                .Where(x => x.overturn_rollover == overturn_rollover)
-                .Where(x => x.pedestrian_involved == pedestrian_involved)
-                .Where(x => x.roadway_departure == roadway_departure)
-                .Where(x => x.route == route)
-                .Where(x => x.single_vehicle == single_vehicle)
-                .Where(x => x.teenage_driver_involved == teenage_driver_involved)
-                .Where(x => x.unrestrained == unrestrained)
-                .Where(x => x.wild_animal_related == wild_animal_related)
-                .Where(x => x.work_zone_related == work_zone_related);
-
-            return record;
+            IQueryable<crash_data> record = _context.Crash_Data;
+            if (bicyclist_involved != null)
+                record = record.Where(x => x.bicyclist_involved == bicyclist_involved);
+            if (city != null)
+                record = record.Where(x => x.city == city || city == null);
+            if (commercial_motor_veh_involved != null)
+                record = record.Where(x => x.commercial_motor_veh_involved == commercial_motor_veh_involved);
+            if (county_name != null)
+                record = record.Where(x => x.county_name == county_name);
+            if (crash_severity_id != null)
+                record = record.Where(x => x.crash_severity_id == crash_severity_id);
+            if (distracted_driving != null)
+                record = record.Where(x => x.distracted_driving == distracted_driving);
+            if (domestic_animal_related != null)
+                record = record.Where(x => x.domestic_animal_related == domestic_animal_related);
+            if (drowsy_driving != null)
+                record = record.Where(x => x.drowsy_driving == drowsy_driving);
+            if (dui != null)
+                record = record.Where(x => x.dui == dui);
+            if (improper_restraint != null)
+                record = record.Where(x => x.improper_restraint == improper_restraint);
+            if (intersection_related != null)
+                record = record.Where(x => x.intersection_related == intersection_related);
+            if (main_road_name != null)
+                record = record.Where(x => x.main_road_name == main_road_name);
+            if (milepoint != null)
+                record = record.Where(x => x.milepoint == milepoint);
+            if (motorcycle_involved != null)
+                record = record.Where(x => x.motorcycle_involved == motorcycle_involved);
+            if (night_dark_condition != null)
+                record = record.Where(x => x.night_dark_condition == night_dark_condition);
+            if (older_driver_involved != null)
+                record = record.Where(x => x.older_driver_involved == older_driver_involved);
+            if (overturn_rollover != null)
+                record = record.Where(x => x.overturn_rollover == overturn_rollover);
+            if (pedestrian_involved != null)
+                record = record.Where(x => x.pedestrian_involved == pedestrian_involved);
+            if (roadway_departure != null)
+                record = record.Where(x => x.roadway_departure == roadway_departure);
+            if (route != null)
+                record = record.Where(x => x.route == route);
+            if (single_vehicle != null)
+                record = record.Where(x => x.single_vehicle == single_vehicle);
+            if (teenage_driver_involved != null)
+                record = record.Where(x => x.teenage_driver_involved == teenage_driver_involved);
+            if (unrestrained != null)
+                record = record.Where(x => x.unrestrained == unrestrained);
+            if (wild_animal_related != null)
+                record = record.Where(x => x.wild_animal_related == wild_animal_related);
+            if(work_zone_related != null)
+                 record = record.Where(x => x.work_zone_related == work_zone_related);
+            return record.ToList();
         }
     }
 }
